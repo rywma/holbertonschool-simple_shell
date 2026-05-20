97dc86f35e6415fbe51793f267ed24359666aa31
noura <noura.fahad2022@gmail.com>
2026-05-20 16:30:51 +0000

    Fix exit and PATH

--- utils.c
+++ utils.c
@@ -7,6 +7,7 @@
  *
  * Return: 0 if equal, positive or negative if different
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2 && *s1 == *s2)
@@ -14,15 +15,17 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
+
 	return (*s1 - *s2);
 }
 
 /**
  * print_env - prints all environment variables
- * @envp: array of environment varuable strings
+ * @envp: array of environment variable strings
  *
  * Return: void
  */
+
 void print_env(char **envp)
 {
 	int i = 0;
@@ -37,25 +40,28 @@ void print_env(char **envp)
 
 /**
  * handle_builtins - checks and runs builtin commands
- * @args: array of argument string
+ * @args: array of argument strings
  * @line: input line
  * @envp: environment variables
  *
  * Return: 1 if builtin was handled, 0 if not
  */
+
 int handle_builtins(char **args, char *line, char **envp)
 {
 	if (_strcmp(args[0], "exit") == 0)
 	{
 		free(args);
 		free(line);
-		exit(0);
+		exit(EXIT_SUCCESS);
 	}
+
 	if (_strcmp(args[0], "env") == 0)
 	{
 		print_env(envp);
 		free(args);
 		return (1);
 	}
+
 	return (0);
 }
